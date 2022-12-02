69404a1109446fbb9281871e05982b7128e7f84a
Henri Michel <henri@tymate.com>
2022-12-02 06:48:27 +0100

    style: switch to reduce syntax :christmas_tree:

--- src/2022/02/index.ts
+++ src/2022/02/index.ts
@@ -4,52 +4,46 @@ import { SolutionFunction } from '../../types';
 export const parseInput = (input: string): string[][] =>
   input.split('\n').map(day => day.split(' '));
 
-export const partOne: SolutionFunction = pipe(parseInput, input => {
-  let score = 0;
-  input.forEach(round => {
-    const [elf, me] = round;
+export const partOne: SolutionFunction = pipe(parseInput, input =>
+  input.reduce((score, [elf, me]) => {
     switch (elf) {
       case 'A':
-        if (me === 'X') score += 1 + 3;
-        if (me === 'Y') score += 2 + 6;
-        if (me === 'Z') score += 3;
+        if (me === 'X') return score + 1 + 3;
+        if (me === 'Y') return score + 2 + 6;
+        if (me === 'Z') return score + 3 + 0;
         break;
       case 'B':
-        if (me === 'X') score += 1;
-        if (me === 'Y') score += 2 + 3;
-        if (me === 'Z') score += 3 + 6;
+        if (me === 'X') return score + 1 + 0;
+        if (me === 'Y') return score + 2 + 3;
+        if (me === 'Z') return score + 3 + 6;
         break;
       case 'C':
-        if (me === 'X') score += 1 + 6;
-        if (me === 'Y') score += 2;
-        if (me === 'Z') score += 3 + 3;
+        if (me === 'X') return score + 1 + 6;
+        if (me === 'Y') return score + 2 + 0;
+        if (me === 'Z') return score + 3 + 3;
         break;
     }
-  });
-  return score;
-});
+  }, 0),
+);
 
-export const partTwo: SolutionFunction = pipe(parseInput, input => {
-  let score = 0;
-  input.forEach(round => {
-    const [elf, me] = round;
+export const partTwo: SolutionFunction = pipe(parseInput, input =>
+  input.reduce((score, [elf, me]) => {
     switch (elf) {
       case 'A':
-        if (me === 'X') score += 0 + 3;
-        if (me === 'Y') score += 3 + 1;
-        if (me === 'Z') score += 6 + 2;
+        if (me === 'X') return score + 0 + 3;
+        if (me === 'Y') return score + 3 + 1;
+        if (me === 'Z') return score + 6 + 2;
         break;
       case 'B':
-        if (me === 'X') score += 0 + 1;
-        if (me === 'Y') score += 3 + 2;
-        if (me === 'Z') score += 6 + 3;
+        if (me === 'X') return score + 0 + 1;
+        if (me === 'Y') return score + 3 + 2;
+        if (me === 'Z') return score + 6 + 3;
         break;
       case 'C':
-        if (me === 'X') score += 0 + 2;
-        if (me === 'Y') score += 3 + 3;
-        if (me === 'Z') score += 6 + 1;
+        if (me === 'X') return score + 0 + 2;
+        if (me === 'Y') return score + 3 + 3;
+        if (me === 'Z') return score + 6 + 1;
         break;
     }
-  });
-  return score;
-});
+  }, 0),
+);
