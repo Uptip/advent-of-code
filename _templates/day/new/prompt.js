21a7a58b09175f6be67f527368a66754331c3b05
Henri Michel <henri@tymate.com>
2022-12-03 15:46:41 +0100

    chore: enable input generation when generating new day :mrs_claus:

--- _templates/day/new/prompt.js
+++ _templates/day/new/prompt.js
@@ -4,15 +4,46 @@ const axios = require('axios');
 const { promises: fs } = require('fs');
 const path = require('path');
 
+const fetchInput = async ({ year, day }) => {
+  const { data } = await axios.get(
+    `https://adventofcode.com/${year}/day/${Number(day)}/input`,
+    {
+      headers: {
+        'Content-Type': 'text/plain; charset=UTF-8',
+        Cookie: `session=${process.env.AOC_SESSION}`,
+      },
+    },
+  );
+
+  try {
+    await fs.mkdir(
+      path.join(__dirname, `../../../src/${year}/${day.padStart(2, '0')}`),
+    );
+  } catch (err) {}
+  await fs.writeFile(
+    path.join(
+      __dirname,
+      `../../../src/${year}/${day.padStart(2, '0')}/input.txt`,
+    ),
+    data.trim(),
+  );
+};
+
 module.exports = {
   prompt: ({ prompter }) => {
-    return new Promise(resolve => {
+    return new Promise(async resolve => {
       if (process.argv.includes('--day')) {
+        const day = process.argv[process.argv.indexOf('--day') + 1].padStart(
+          2,
+          '0',
+        );
+        const year =
+          process.argv[process.argv.indexOf('--year') + 1] ||
+          new Date().getFullYear();
+        await generateInput({ day, year });
         return resolve({
-          day: process.argv[process.argv.indexOf('--day') + 1].padStart(2, '0'),
-          year:
-            process.argv[process.argv.indexOf('--year') + 1] ||
-            new Date().getFullYear(),
+          day,
+          year,
         });
       }
 
@@ -32,34 +63,10 @@ module.exports = {
           },
         ])
         .then(async ({ day, year }) => {
-          const { data } = await axios.get(
-            `https://adventofcode.com/${year}/day/${Number(day)}/input`,
-            {
-              headers: {
-                'Content-Type': 'text/plain; charset=UTF-8',
-                Cookie: `session=${process.env.AOC_SESSION}`,
-              },
-            },
-          );
-
-          try {
-            await fs.mkdir(
-              path.join(
-                __dirname,
-                `../../../src/${year}/${day.padStart(2, '0')}`,
-              ),
-            );
-          } catch (err) {}
-          await fs.writeFile(
-            path.join(
-              __dirname,
-              `../../../src/${year}/${day.padStart(2, '0')}/input.txt`,
-            ),
-            data.trim(),
-          );
-
+          day = day.padStart(2, '0');
+          await generateInput({ day, year });
           resolve({
-            day: day.padStart(2, '0'),
+            day,
             year,
           });
         });
