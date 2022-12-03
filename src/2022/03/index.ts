ab595d14c3d5c673c818e9864e33dc111a8cee9a
Henri Michel <henri@tymate.com>
2022-12-03 06:27:37 +0100

    feat: complete 2022 day 3 :christmas_tree:

---
+++ src/2022/03/index.ts
@@ -0,0 +1,41 @@
+import { pipe } from 'ramda';
+import { SolutionFunction } from '../../types';
+import { chunk, sum } from 'lodash';
+
+export const parseInput = (input: string): string[][] =>
+  input
+    .split('\n')
+    .map(rucksack =>
+      chunk(rucksack, rucksack.length / 2).map(row => row.join('')),
+    );
+
+const scores = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+export const partOne: SolutionFunction = pipe(
+  parseInput,
+  input =>
+    input
+      .map(compartments =>
+        compartments[0]
+          .split('')
+          .find(letter => compartments[1].includes(letter)),
+      )
+      .map(letter => scores.indexOf(letter) + 1),
+  sum,
+);
+
+export const partTwo: SolutionFunction = pipe(
+  parseInput,
+  input =>
+    chunk(input, 3)
+      .map((group: string[][]) => {
+        const elves = group.map(elf => elf.join(''));
+        return elves[0]
+          .split('')
+          .find(
+            letter => elves[1].includes(letter) && elves[2].includes(letter),
+          );
+      })
+      .map(letter => scores.indexOf(letter) + 1),
+  sum,
+);
