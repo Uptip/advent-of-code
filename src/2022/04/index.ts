71c9ffad8dcfd2b3b18a909204589a1dfdd605ad
Henri Michel <henri@tymate.com>
2022-12-04 06:17:48 +0100

    style: switch parseInt to Number :cookie:

--- src/2022/04/index.ts
+++ src/2022/04/index.ts
@@ -4,8 +4,8 @@ export const parseInput = (input: string) =>
   input.split('\n').map(line => {
     const [, from1, to1, from2, to2] = line.match(/^(\d+)-(\d+),(\d+)-(\d+)/);
     return {
-      elf1: { from: parseInt(from1), to: parseInt(to1) },
-      elf2: { from: parseInt(from2), to: parseInt(to2) },
+      elf1: { from: Number(from1), to: Number(to1) },
+      elf2: { from: Number(from2), to: Number(to2) },
     };
   });
 
