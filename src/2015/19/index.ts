057bc545886273f35c83bf963ef606ad80967e41
Henri Michel <henri@tymate.com>
2022-12-22 21:40:53 +0100

    feat: add 2015 day 19 :snowman:

--- src/2015/19/index.ts
+++ src/2015/19/index.ts
@@ -1,27 +1,52 @@
 import { pipe } from 'ramda';
 
-export const parseInput = (
-  input: string,
-): { replacements: { [element: string]: string[] }; molecule: string } => ({
-  replacements: input
-    .split('\n')
-    .slice(0, -2)
-    .reduce((formula, line) => {
-      const [element, replacement] = line.split(' => ');
+export const parseInput = (input: string) => {
+  const [rawReplacements, formula] = input.split('\n\n');
 
-      if (!formula[element]) formula[element] = [];
-      formula[element].push(replacement);
+  const replacements = rawReplacements.split('\n').reduce((acc, line) => {
+    const [from, to] = line.split(' => ');
+    return { ...acc, [from]: [...(acc[from] || []), to] };
+  }, {});
+  return { replacements, formula };
+};
 
-      return formula;
-    }, {}),
-  molecule: input.split('\n').slice(-1)[0],
+export const partOne = pipe(parseInput, ({ replacements, formula }) => {
+  let match: RegExpExecArray | null;
+  let matches = [];
+  while ((match = /([a-zA-Z][a-z]*)/g.exec(formula)) !== null) {
+    matches.push(match);
+  }
+  return new Set([
+    ...matches.reduce((data, entry) => {
+      const index = entry.index;
+      const element = entry[0];
+      if (replacements[element]) {
+        replacements[element].forEach(replacement => {
+          data.push(`${formula}`.splice(index, element.length, replacement));
+        });
+      }
+      return data;
+    }, []),
+  ]).size;
 });
 
-export const partOne = pipe(parseInput, input => {
-  console.log(input);
-  return 0;
-});
+export const partTwo = pipe(parseInput, ({ replacements, formula }) => {
+  const reverse = Object.keys(replacements).reduce((table, replacement) => {
+    replacements[replacement].forEach((element: any) => {
+      table.set(element, replacement);
+    });
+    return table;
+  }, new Map());
 
-export const partTwo = pipe(parseInput, input => {
-  return 0;
+  let total = 0;
+
+  while (formula !== 'e') {
+    for (const [element, replacement] of reverse.entries()) {
+      if (formula.includes(element)) {
+        formula = formula.replace(element, replacement);
+        total += 1;
+      }
+    }
+  }
+  return total;
 });
